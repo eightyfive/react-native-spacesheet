1fed162f61f80aa143ece6f846efae6bfe6f997f
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-01-04 17:21:28 +0100

    Remove strategies

--- index.test.js
+++ index.test.js
@@ -1,28 +1,5 @@
 import SpaceSheet from './index';
 
-describe('SpaceSheet(amount, length, strategy)', () => {
-  const space = SpaceSheet.create(5, 6, 'double');
-  // sizes = [0, 5, 10, 20, 40, 80]
-  //         [0, 1,  2,  3,  4,  5]
-
-  test('Aliases', () => {
-    expect(space.styles.mt2).toHaveProperty('marginTop', 10);
-    expect(space.styles.pb3).toHaveProperty('paddingBottom', 20);
-    expect(space.styles.mv5).toHaveProperty('marginVertical', 80);
-    expect(space.styles.ph0).toHaveProperty('paddingHorizontal', 0);
-  });
-
-  test('Wrong alias', () => {
-    // TODO: Needs to throw Error when real Proxy support
-    expect(space.styles.fooBar).toBeUndefined();
-  });
-
-  test('Wrong size', () => {
-    // TODO: Needs to throw Error when real Proxy support
-    expect(space.styles.m0123456789).toBeUndefined();
-  });
-});
-
 describe('SpaceSheet(sizes)', () => {
   const space = SpaceSheet.create([0, 4, 8, 16]);
   // sizes = [0, 4, 8, 16]
--- strategies.js
+++
@@ -1,7 +0,0 @@
-export function double(index, amount) {
-  return index ? amount * Math.pow(2, index - 1) : 0;
-}
-
-export function linear(index, amount) {
-  return amount * index;
-}
